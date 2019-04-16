9fe2eaa7d854e4743884dfcb6a152da94d2a7dd9
Benjamin Lepine <benjamin.lepine@epitech.eu>
2019-04-16 20:45:11 +0200

    all binding commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularblog';
+  isAuth = false;
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+
+  constructor(){
+    setTimeout(
+      ()=>{
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer(){
+    console.log("tototo")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {getElementDepthCount} from "@angular/core/src/render3/state";
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
